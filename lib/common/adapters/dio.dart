1967dc2df337cefc86f15fb9ca7c5a7c824b3bda
João <joao.coelho@vrsoft.com.br>
2025-04-09 11:10:06 -0300

    refactor: Ajustar dio adapter

--- lib/common/adapters/dio.dart
+++ lib/common/adapters/dio.dart
@@ -4,19 +4,19 @@ import 'package:dio/dio.dart';
 
 class DioHttpClient implements IHttpClient {
   final Dio _dio;
-  DioHttpClient(this._dio);
+  DioHttpClient(this._dio) {
+    _dio.options.validateStatus = (status) {
+      return status != null && status >= 200 && status < 500;
+    };
+  }
 
   @override
   Future<HttpResponse> delete(String url, {Map<String, String>? headers}) async {
     try {
       final response = await _dio.delete(url, options: Options(headers: headers));
-      return HttpResponse(
-        statusCode: response.statusCode ?? 200,
-        statusMessage: response.statusMessage ?? '',
-        data: response.data['data'],
-      );
+      return HttpResponse(statusCode: response.statusCode!, data: response.data['data']);
     } catch (e) {
-      return HttpResponse(statusCode: 500, statusMessage: e.toString(), data: null);
+      return _handleError(e);
     }
   }
 
@@ -24,13 +24,9 @@ class DioHttpClient implements IHttpClient {
   Future<HttpResponse> get(String url, {Map<String, String>? headers}) async {
     try {
       final response = await _dio.get(url, options: Options(headers: headers));
-      return HttpResponse(
-        statusCode: response.statusCode ?? 200,
-        statusMessage: response.statusMessage ?? '',
-        data: response.data['data'],
-      );
+      return HttpResponse(statusCode: response.statusCode!, data: response.data['data']);
     } catch (e) {
-      return HttpResponse(statusCode: 500, statusMessage: e.toString(), data: null);
+      return _handleError(e);
     }
   }
 
@@ -38,13 +34,9 @@ class DioHttpClient implements IHttpClient {
   Future<HttpResponse> post(String url, {Map<String, String>? headers, body}) async {
     try {
       final response = await _dio.post(url, options: Options(headers: headers), data: body);
-      return HttpResponse(
-        statusCode: response.statusCode ?? 200,
-        statusMessage: response.statusMessage ?? '',
-        data: response.data['data'],
-      );
+      return HttpResponse(statusCode: response.statusCode!, data: response.data['data']);
     } catch (e) {
-      return HttpResponse(statusCode: 500, statusMessage: e.toString(), data: null);
+      return _handleError(e);
     }
   }
 
@@ -52,13 +44,20 @@ class DioHttpClient implements IHttpClient {
   Future<HttpResponse> put(String url, {Map<String, String>? headers, body}) async {
     try {
       final response = await _dio.put(url, options: Options(headers: headers), data: body);
-      return HttpResponse(
-        statusCode: response.statusCode ?? 200,
-        statusMessage: response.statusMessage ?? '',
-        data: response.data['data'],
-      );
+      return HttpResponse(statusCode: response.statusCode!, data: response.data['data']);
     } catch (e) {
-      return HttpResponse(statusCode: 500, statusMessage: e.toString(), data: null);
+      return _handleError(e);
     }
+  }
+
+  HttpResponse _handleError(Object error) {
+    var statusCode = 500;
+    dynamic data;
+    if (error is DioException) {
+      statusCode = error.response?.statusCode ?? 500;
+      data = error.response?.data['data'];
+    }
+
+    return HttpResponse(statusCode: statusCode, data: data);
   }
 }
--- lib/common/dtos/http_response.dart
+++ lib/common/dtos/http_response.dart
@@ -1,12 +1,11 @@
 class HttpResponse {
   final int statusCode;
-  final String statusMessage;
   final dynamic data;
 
-  HttpResponse({required this.statusCode, required this.statusMessage, required this.data});
+  HttpResponse({required this.statusCode, required this.data});
 
   @override
   String toString() {
-    return 'HttpResponse(statusCode: $statusCode, statusMessage: $statusMessage, data: $data)';
+    return 'HttpResponse(statusCode: $statusCode, data: $data)';
   }
 }
